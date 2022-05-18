17d73575a6ceaf715d6640b5cdb73585f199158a
cheenaeng <cheena94sing@gmail.com>
2022-05-18 16:42:29 +0800

    added create trip input

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,8 @@
 import axios from 'axios';
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import {
+  BrowserRouter as Router, Routes, Route, Link,
+} from 'react-router-dom';
 import './App.css';
 import AllTripsDisplay from './Pages/AllTripsDisplay.jsx';
 import CreateTrip from './Pages/CreateTrip.jsx';
@@ -10,6 +12,10 @@ axios.defaults.withCredentials = true;
 function App() {
   return (
     <Router>
+      <nav>
+        <Link to="/"> Home</Link>
+        <Link to="/createTrip"> Create Trip</Link>
+      </nav>
       <Routes>
         <Route path="/" element={<AllTripsDisplay />}> </Route>
         <Route path="/createTrip" element={<CreateTrip />}> </Route>
--- src/Pages/CreateTrip.jsx
+++ src/Pages/CreateTrip.jsx
@@ -1,8 +1,26 @@
-import React from 'react';
+import React, { useState } from 'react';
+import axios from 'axios';
 
 function CreateTrip() {
+  const [tempTripName, setTripName] = useState('');
+  const handleTripNameChange = (e) => {
+    setTripName(e.target.value);
+  };
+
+  const recordTrip = () => {
+    axios.post('/createTrip').then((response) => {
+      console.log(response.data);
+    });
+    setTripName('');
+  };
   return (
-    <div>TO CREATE TRIP PAGE</div>
+    <>
+      <h1> Create a trip </h1>
+      <span> Trip Name: </span>
+      <input type="text" placeholder="Key in trip name" id="tripName" name="tripName" onChange={handleTripNameChange} value={tempTripName} />
+      <button type="button" onClick={recordTrip}> Enter</button>
+    </>
+
   );
 }
 
