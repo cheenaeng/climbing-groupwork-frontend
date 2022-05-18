a3858ccbb1d40ec845f74832ca13cf63f89de64b
cheenaeng <cheena94sing@gmail.com>
2022-05-19 00:33:10 +0800

    added trip display and reducer,context

--- src/App.jsx
+++ src/App.jsx
@@ -5,19 +5,22 @@ import {
 import './App.css';
 import AllTripsDisplay from './Pages/AllTripsDisplay.jsx';
 import CreateTrip from './Pages/CreateTrip.jsx';
+import { TripProvider } from './tripsActions.js';
 
 function App() {
   return (
-    <Router>
-      <nav>
-        <Link to="/"> Home</Link>
-        <Link to="/createTrip"> Create Trip</Link>
-      </nav>
-      <Routes>
-        <Route path="/" element={<AllTripsDisplay />}> </Route>
-        <Route path="/createTrip" element={<CreateTrip />}> </Route>
-      </Routes>
-    </Router>
+    <TripProvider>
+      <Router>
+        <nav>
+          <Link to="/"> Home</Link>
+          <Link to="/createTrip"> Create Trip</Link>
+        </nav>
+        <Routes>
+          <Route path="/" element={<AllTripsDisplay />}> </Route>
+          <Route path="/createTrip" element={<CreateTrip />}> </Route>
+        </Routes>
+      </Router>
+    </TripProvider>
   );
 }
 
