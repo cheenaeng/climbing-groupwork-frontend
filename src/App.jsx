1c802ec00411717e019e76a40b06258aae1eaa64
cheenaeng <cheena94sing@gmail.com>
2022-05-18 12:43:36 +0800

    changed filed names, added necessary connection to backend, eslint install

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,8 @@
+import axios from 'axios';
 import logo from './logo.svg';
 import './App.css';
+
+axios.defaults.withCredentials = true;
 
 function App() {
   return (
@@ -7,7 +10,11 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Edit
+          {' '}
+          <code>src/App.js</code>
+          {' '}
+          and save to reload.
         </p>
         <a
           className="App-link"
